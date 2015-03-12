d60cfbef61d3623f93040609596b3d63a6245a99
James Ring <sjr@google.com>
2015-03-12 16:04:37 -0700

    v23/services/security/access: rename ACL to AccessList
    Change-Id: I629fd72d8b88b3bb44ac7169c73b97441f118cad

--- v23/services/security/access/util.go
+++ v23/services/security/access/util.go
@@ -13,11 +13,11 @@ import (
 // #include "jni.h"
 import "C"
 
-// JavaACL converts the provided Go ACL into a Java ACL.
+// JavaACL converts the provided Go AccessList into a Java AccessList.
 // NOTE: Because CGO creates package-local types and because this method may be
 // invoked from a different package, Java types are passed in an empty interface
 // and then cast into their package local types.
-func JavaACL(jEnv interface{}, acl access.ACL) (unsafe.Pointer, error) {
+func JavaACL(jEnv interface{}, acl access.AccessList) (unsafe.Pointer, error) {
 	encoded, err := vom.Encode(acl)
 	if err != nil {
 		return nil, err
@@ -29,27 +29,27 @@ func JavaACL(jEnv interface{}, acl access.ACL) (unsafe.Pointer, error) {
 	return jACL, nil
 }
 
-// GoACL converts the provided Java ACL into a Go ACL.
+// GoACL converts the provided Java AccessList into a Go AccessList.
 // NOTE: Because CGO creates package-local types and because this method may be
 // invoked from a different package, Java types are passed in an empty interface
 // and then cast into their package local types.
-func GoACL(jEnv, jACL interface{}) (access.ACL, error) {
+func GoACL(jEnv, jACL interface{}) (access.AccessList, error) {
 	encoded, err := jutil.CallStaticByteArrayMethod(jEnv, jUtilClass, "encodeACL", []jutil.Sign{aclSign}, jACL)
 	if err != nil {
-		return access.ACL{}, err
+		return access.AccessList{}, err
 	}
-	var a access.ACL
+	var a access.AccessList
 	if err := vom.Decode(encoded, &a); err != nil {
-		return access.ACL{}, err
+		return access.AccessList{}, err
 	}
 	return a, nil
 }
 
-// JavaACLWrapper converts the provided Go ACL into a Java ACLWrapper object.
+// JavaACLWrapper converts the provided Go AccessList into a Java ACLWrapper object.
 // NOTE: Because CGO creates package-local types and because this method may be
 // invoked from a different package, Java types are passed in an empty interface
 // and then cast into their package local types.
-func JavaACLWrapper(jEnv interface{}, acl access.ACL) (unsafe.Pointer, error) {
+func JavaACLWrapper(jEnv interface{}, acl access.AccessList) (unsafe.Pointer, error) {
 	jACL, err := JavaACL(jEnv, acl)
 	if err != nil {
 		return nil, err
